e73626368f9f20c75dca23bb4b545461ca9f335f
web-padawan <iamkulykov@gmail.com>
2020-12-01 16:02:19 +0200

    Cleanup Polymer imports

--- src/vaadin-item.js
+++ src/vaadin-item.js
@@ -3,12 +3,11 @@
 Copyright (c) 2017 Vaadin Ltd.
 This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
 */
-import { PolymerElement } from '@polymer/polymer/polymer-element.js';
-
+import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 import { DirMixin } from '@vaadin/vaadin-element-mixin/vaadin-dir-mixin.js';
 import { ItemMixin } from './vaadin-item-mixin.js';
-import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+
 /**
  * `<vaadin-item>` is a Web Component providing layout for items in tabs and menus.
  *
